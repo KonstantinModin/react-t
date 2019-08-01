a89c123fa52adabd8e08bc96b00d27e06374f1c0
Konstantin <konstantin.v.modin@gmail.com>
2019-08-01 06:08:55 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import Clock from './components/Clock';
+import Header from './components/Header';
+import Drums from './components/Drums';
+import Back from './45621.jpg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+    const keyHandler = (d) => {
+        console.log('key Down', d.keyCode);
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', keyHandler);
+    }, []);
+
+    return (
+        <div className="App">        
+            <Route path="/" component={Header} />
+
+            <div className="Background" >
+                <img src={Back} alt="background"/>
+            </div>
+
+            <div className="Container">            
+                <Route exact path="/" render={() => <h2>Choose some menu item provided:</h2>} />        
+                <Route path="/clock" component={Clock} />
+                <Route path="/drums" component={Drums} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
