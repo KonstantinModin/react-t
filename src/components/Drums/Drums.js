872541359407e2709591c4ec54a315a1de4fcd82
Konstantin <konstantin.v.modin@gmail.com>
2019-08-01 20:07:54 +0200

    updates

--- src/components/Drums/Drums.js
+++ src/components/Drums/Drums.js
@@ -1,7 +1,15 @@
 import React, { useEffect } from 'react';
 import Key from './Key';
 import './Drums.css';
-import sound from './audio/boom.wav';
+import clap from './audio/clap.mp3';
+import hihat from './audio/hihat.wav';
+import kick from './audio/kick.wav';
+import openhat from './audio/openhat.wav';
+import boom from './audio/boom.wav';
+import hey from './audio/hey.wav';
+import screech from './audio/screech.wav';
+import hammock from './audio/hammock.wav';
+import triangle from './audio/triangle.wav';
 
 const Drums = () => {
     const keys = [
@@ -10,20 +18,31 @@ const Drums = () => {
         {dataKey: 68, key: 'D', label: 'KICK'},
         {dataKey: 70, key: 'F', label: 'OPENHAT'},
         {dataKey: 71, key: 'G', label: 'BOOM'},
-        {dataKey: 72, key: 'H', label: 'RIDE'},
-        {dataKey: 74, key: 'J', label: 'SNARE'},
-        {dataKey: 75, key: 'K', label: 'TOM'},
-        {dataKey: 76, key: 'L', label: 'TINK'},
+        {dataKey: 72, key: 'H', label: 'HEY'},
+        {dataKey: 74, key: 'J', label: 'SCREECH'},
+        {dataKey: 75, key: 'K', label: 'HAMMOCK'},
+        {dataKey: 76, key: 'L', label: 'TRIANGLE'},
     ];
+    const sounds = {
+        65: clap,
+        83: hihat,
+        68: kick,
+        70: openhat,
+        71: boom,
+        72: hey,
+        74: screech,
+        75: hammock,
+        76: triangle,
+    }
 
-    const Boom = new Audio(sound);
-
-
+    
+    
     const keyHandler = (d) => {
-        console.log('key Down', d.keyCode);
-        Boom.currentTime = 0;
-        Boom.play();
-        console.log('boom :', Boom, typeof Boom);
+        const sound = new Audio(sounds[d.keyCode]);
+        console.log('key Down', d.keyCode, sounds);
+        sound.currentTime = 0;
+        sound.play();
+        console.log('boom :', sound, typeof sound);
     }
 
     useEffect(() => {
