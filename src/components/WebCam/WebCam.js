6528612dc3c21273fcf8014bcf0f21d2a793612d
Konstantin <konstantin.v.modin@gmail.com>
2019-08-05 07:25:05 +0200

    updates

--- src/components/WebCam/WebCam.js
+++ src/components/WebCam/WebCam.js
@@ -1,32 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './WebCam.css';
 
 const WebCam = () => {
+    const videoRef = React.createRef();    
+    
+    const getVideo = () => {
+        navigator.mediaDevices.getUserMedia({video: true, audio: false})
+            .then(stream => {
+                console.log(stream);                
+                videoRef.current.srcObject = stream;
+                videoRef.current.play();
+                
+            })
+            .catch(err => console.error(err));
+    };
+
+    useEffect(() => {
+        console.log('videoRef :', videoRef);
+        getVideo();
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <div className="WebCam">
             <h1>WebCam</h1>
             <div className="Controls">
                 <button type="button" className="btn btn-success">Take Photo</button>
                 <div className="RGB">
-                    <label for="rmin">Red Min:</label>
+                    <label htmlFor="rmin">Red Min:</label>
                     <input type="range" min={0} max={255} name="rmin" />
-                    <label for="rmax">Red Max:</label>
+                    <label htmlFor="rmax">Red Max:</label>
                     <input type="range" min={0} max={255} name="rmax" />
                     <br />
-                    <label for="gmin">Green Min:</label>
+                    <label htmlFor="gmin">Green Min:</label>
                     <input type="range" min={0} max={255} name="gmin" />
-                    <label for="gmax">Green Max:</label>
+                    <label htmlFor="gmax">Green Max:</label>
                     <input type="range" min={0} max={255} name="gmax" />
                     <br />
-                    <label for="bmin">Blue Min:</label>
+                    <label htmlFor="bmin">Blue Min:</label>
                     <input type="range" min={0} max={255} name="bmin" />
-                    <label for="bmax">Blue Max:</label>
+                    <label htmlFor="bmax">Blue Max:</label>
                     <input type="range" min={0} max={255} name="bmax" />
                     <br />
                 </div>
             </div>
             <canvas className="Photo"></canvas>
-            <video className="Player"></video>
+            <video                 
+                className="Player"
+                ref={videoRef}
+            />
             <div className="Strip"></div>
         </div>
     )
