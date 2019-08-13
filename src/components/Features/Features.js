ed56577b17e05478c2f8b41027474c9170418818
Konstantin <konstantin.v.modin@gmail.com>
2019-08-13 06:26:20 +0200

    updates

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,27 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Features.css';
 
 const Features = () => {
     const block1 = React.createRef();
     const shadowText = React.createRef();
 
-    const shadow = (e) => {
-        const walk = 100;
-        const blur = 3;
+    const [ mouseIn, setMouseIn ] = useState(false);
+    const [xCircle, setXCircle] = useState(-50);
 
-        console.dir(e.target);
+    useEffect(() => {
+        const cycle = setInterval(() => {
+            console.log('mouseIn :', mouseIn);
+            if (!mouseIn) {// let x = -50;
+                console.log('begin', xCircle);
+                // debugger;
+                let r = 50;
+                let y = Math.sqrt(r*r - xCircle*xCircle);
+                if (shadowText.current) draw(xCircle, y);
+                setXCircle(prev => {
+                    console.log('state :', prev);
+                    return prev > 50 ? -50 : prev + 1
+                });
+                
+                console.log('end', xCircle, y);
+                // debugger;
+            }
+        }, 1000);
+        return () => {
+            clearInterval(cycle);
+        }
+    }, []);
+
+
+
+    const mouseMoveHandler = (e) => {
+        const walk = 100;        
+
         const { offsetWidth: width, offsetHeight: height } = block1.current;
         let { offsetX: x, offsetY: y } = e;
         if (e.target.localName === 'h1') {
             x += e.target.offsetLeft;
             y += e.target.offsetTop;
         }
-        // console.log('width, height :', width, height);
-        // console.log('x, y :', x, y);
+       
         const xWalk = ~~((x / width * walk) - walk / 2);
         const yWalk = ~~((y / height * walk) - walk / 2);
+        
+        console.dir(e.target);
+        console.log('x, y :', x, y);
         console.log(~~xWalk, ~~yWalk);
-        // console.log()
+
+        draw(xWalk, yWalk);
+        
+    }
+
+    const draw = (xWalk, yWalk) => {
+        const blur = 3;
         shadowText.current.style.textShadow = `
             ${xWalk}px ${yWalk}px ${blur}px rgba(255,0,255,0.7),
             ${xWalk * -1}px ${yWalk}px ${blur}px rgba(0,255,255,0.7),
@@ -32,9 +66,16 @@ const Features = () => {
     
     return (
         <div className="Features">
-            <h1>Features</h1>
+            <h1>Features{xCircle}{mouseIn.toString()}</h1>
             <div className="Grid">
-                <div className="Block1" onMouseMove={(e) => shadow(e.nativeEvent)} ref={block1}>
+                {/* <span>{mouseIn.toString()}</span> */}
+                <div 
+                    className="Block1" 
+                    onMouseMove={(e) => mouseMoveHandler(e.nativeEvent)}
+                    onMouseEnter={() => setMouseIn(true)} 
+                    onMouseLeave={() => setMouseIn(false)}
+                    ref={block1}
+                >
                     <div className="innerDiv"><h1 ref={shadowText}>Shadow<br/>Effect</h1></div>
                 </div>
                 <div className="Block2">
