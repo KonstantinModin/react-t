4a4669178f0c81166b24c77732ac7a6bd79f2cd6
Konstantin <konstantin.v.modin@gmail.com>
2019-08-13 08:29:26 +0200

    updates

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,44 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import './Features.css';
 
-const Features = () => {
-    const block1 = React.createRef();
-    const shadowText = React.createRef();
-
-    const [ mouseIn, setMouseIn ] = useState(false);
-    const [xCircle, setXCircle] = useState(-50);
-
-    const calculateCircle = () => {
-        console.log('mouseIn :', mouseIn);
-        if (!mouseIn) {// let x = -50;
-            console.log('begin', xCircle);
-            // debugger;
-            let r = 50;
-            let y = Math.sqrt(r*r - xCircle*xCircle);
-            if (shadowText.current) draw(xCircle, y);
-            setXCircle(prev => {
-                console.log('state :', prev);
-                return prev > 50 ? -50 : prev + 1
-            });
-            
-            console.log('end', xCircle, y);
+class Features extends Component {
+    constructor(props) {
+        super(props);
+        this.block1 = React.createRef();
+        this.shadowText = React.createRef();
+        this.state = {
+            mouseIn: false,
+            xCircle: -50,
+            xDirection: 1
         }
-            // debugger;
     }
 
-    useEffect(() => {
-        const cycle = setInterval(() => calculateCircle(), 1000);
-        return () => {
-            clearInterval(cycle);
-        }
-    }, []);
+    calculateCircle = () => {
+        const { mouseIn, xCircle, xDirection } = this.state;
 
-    
+        if (!mouseIn) {
+            let r = 50;
+            let yCircle = Math.sqrt(r * r - xCircle * xCircle) * xDirection;
+            if (this.shadowText.current) this.draw(xCircle, yCircle);
 
-    const mouseMoveHandler = (e) => {
+            const nextXCircle = xCircle + 1 * xDirection;            
+            this.setState({xCircle: nextXCircle}, () => {
+                if (nextXCircle > 49 || nextXCircle < -49) this.setState({xDirection: xDirection * -1});
+            });
+        }            
+    }
+
+    componentDidMount() {
+        this.cycle = setInterval(() => this.calculateCircle(), 10);        
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.cycle);           
+    }    
+
+    mouseMoveHandler = (e) => {
         const walk = 100;        
 
-        const { offsetWidth: width, offsetHeight: height } = block1.current;
+        const { offsetWidth: width, offsetHeight: height } = this.block1.current;
         let { offsetX: x, offsetY: y } = e;
         if (e.target.localName === 'h1') {
             x += e.target.offsetLeft;
@@ -48,17 +49,15 @@ const Features = () => {
         const xWalk = ~~((x / width * walk) - walk / 2);
         const yWalk = ~~((y / height * walk) - walk / 2);
         
-        console.dir(e.target);
-        console.log('x, y :', x, y);
-        console.log(~~xWalk, ~~yWalk);
-
-        draw(xWalk, yWalk);
-        
+        // console.dir(e.target);
+        // console.log('x, y :', x, y);
+        // console.log(~~xWalk, ~~yWalk);
+        this.draw(xWalk, yWalk);        
     }
 
-    const draw = (xWalk, yWalk) => {
+    draw = (xWalk, yWalk) => {
         const blur = 3;
-        shadowText.current.style.textShadow = `
+        this.shadowText.current.style.textShadow = `
             ${xWalk}px ${yWalk}px ${blur}px rgba(255,0,255,0.7),
             ${xWalk * -1}px ${yWalk}px ${blur}px rgba(0,255,255,0.7),
             ${yWalk}px ${xWalk * - 1}px ${blur}px rgba(0,255,0,0.7),
@@ -66,38 +65,39 @@ const Features = () => {
         `;
     }
     
-    return (
-        <div className="Features">
-            <h1>Features{xCircle}{mouseIn.toString()}</h1>
-            <div className="Grid">
-                {/* <span>{mouseIn.toString()}</span> */}
-                <div 
-                    className="Block1" 
-                    onMouseMove={(e) => mouseMoveHandler(e.nativeEvent)}
-                    onMouseEnter={() => setMouseIn(true)} 
-                    onMouseLeave={() => setMouseIn(false)}
-                    ref={block1}
-                >
-                    <div className="innerDiv"><h1 ref={shadowText}>Shadow<br/>Effect</h1></div>
+    render() {        
+        return (
+            <div className="Features">
+                <h1>Features</h1>                
+                <div className="Grid">                    
+                    <div 
+                        className="Block1" 
+                        onMouseMove={(e) => this.mouseMoveHandler(e.nativeEvent)}
+                        onMouseEnter={() => this.setState({mouseIn: true})} 
+                        onMouseLeave={() => this.setState({mouseIn: false})}
+                        ref={this.block1}
+                    >
+                        <div className="innerDiv"><h1 ref={this.shadowText}>Shadow<br/>Effect</h1></div>
+                    </div>
+                    <div className="Block2">
+                        <div className="innerDiv">2</div>
+                    </div>
+                    <div className="Block3">
+                        <div className="innerDiv">3</div>
+                    </div>
+                    <div className="Block4">
+                        <div className="innerDiv">4</div>
+                    </div>
+                    <div className="Block5">
+                        <div className="innerDiv">5</div>
+                    </div>
+                    <div className="Block6">
+                        <div className="innerDiv">6</div>
+                    </div>                
                 </div>
-                <div className="Block2">
-                    <div className="innerDiv">2</div>
-                </div>
-                <div className="Block3">
-                    <div className="innerDiv">3</div>
-                </div>
-                <div className="Block4">
-                    <div className="innerDiv">4</div>
-                </div>
-                <div className="Block5">
-                    <div className="innerDiv">5</div>
-                </div>
-                <div className="Block6">
-                    <div className="innerDiv">6</div>
-                </div>                
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Features;
