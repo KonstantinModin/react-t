c3368d312332bcc91321c61cc40dd82edbc77adf
Konstantin <konstantin.v.modin@gmail.com>
2019-08-14 07:09:50 +0200

    updates

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,103 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Shadow from './Shadow';
 import './Features.css';
 
-class Features extends Component {
-    constructor(props) {
-        super(props);
-        this.block1 = React.createRef();
-        this.shadowText = React.createRef();
-        this.state = {
-            mouseIn: false,
-            xCircle: -50,
-            xDirection: 1
-        }
-    }
-
-    calculateCircle = () => {
-        const { mouseIn, xCircle, xDirection } = this.state;
-
-        if (!mouseIn) {
-            let r = 50;
-            let yCircle = Math.sqrt(r * r - xCircle * xCircle) * xDirection;
-            if (this.shadowText.current) this.draw(xCircle, yCircle);
-
-            const nextXCircle = xCircle + 1 * xDirection;            
-            this.setState({xCircle: nextXCircle}, () => {
-                if (nextXCircle > 49 || nextXCircle < -49) this.setState({xDirection: xDirection * -1});
-            });
-        }            
-    }
-
-    componentDidMount() {
-        this.cycle = setInterval(() => this.calculateCircle(), 10);        
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.cycle);           
-    }    
-
-    mouseMoveHandler = (e) => {
-        const walk = 100;        
-
-        const { offsetWidth: width, offsetHeight: height } = this.block1.current;
-        let { offsetX: x, offsetY: y } = e;
-        if (e.target.localName === 'h1') {
-            x += e.target.offsetLeft;
-            y += e.target.offsetTop;
-        }
-       
-        const xWalk = ~~((x / width * walk) - walk / 2);
-        const yWalk = ~~((y / height * walk) - walk / 2);
-        
-        // console.dir(e.target);
-        // console.log('x, y :', x, y);
-        // console.log(~~xWalk, ~~yWalk);
-        this.draw(xWalk, yWalk);        
-    }
-
-    draw = (xWalk, yWalk) => {
-        const blur = 3;
-        this.shadowText.current.style.textShadow = `
-            ${xWalk}px ${yWalk}px ${blur}px rgba(255,0,255,0.7),
-            ${xWalk * -1}px ${yWalk}px ${blur}px rgba(0,255,255,0.7),
-            ${yWalk}px ${xWalk * - 1}px ${blur}px rgba(0,255,0,0.7),
-            ${yWalk * -1}px ${xWalk}px ${blur}px rgba(0,0,255,0.7)
-        `;
-    }
-    
-    render() {        
-        return (
-            <div className="Features">
-                <h1>Features</h1>                
-                <div className="Grid">                    
-                    <div 
-                        className="Block1" 
-                        onMouseMove={(e) => this.mouseMoveHandler(e.nativeEvent)}
-                        onMouseEnter={() => this.setState({mouseIn: true})} 
-                        onMouseLeave={() => this.setState({mouseIn: false})}
-                        ref={this.block1}
-                    >
-                        <div className="innerDiv"><h1 ref={this.shadowText}>Shadow<br/>Effect</h1></div>
-                    </div>
-                    <div className="Block2">
-                        <div className="innerDiv">2</div>
-                    </div>
-                    <div className="Block3">
-                        <div className="innerDiv">3</div>
-                    </div>
-                    <div className="Block4">
-                        <div className="innerDiv">4</div>
-                    </div>
-                    <div className="Block5">
-                        <div className="innerDiv">5</div>
-                    </div>
-                    <div className="Block6">
-                        <div className="innerDiv">6</div>
-                    </div>                
+const Features = () => {
+    return (
+        <div className="Features">
+            <h1>Features</h1>                
+            <div className="Grid">
+                <Shadow />                    
+                <div className="Block2">
+                    <div className="innerDiv">2</div>
                 </div>
+                <div className="Block3">
+                    <div className="innerDiv">3</div>
+                </div>
+                <div className="Block4">
+                    <div className="innerDiv">4</div>
+                </div>
+                <div className="Block5">
+                    <div className="innerDiv">5</div>
+                </div>
+                <div className="Block6">
+                    <div className="innerDiv">6</div>
+                </div>                
             </div>
-        )
-    }
+        </div>
+    )    
 }
 
 export default Features;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
 
-const Home = () => <h1 className="Home">Choose some menu item provided above</h1>
+const Home = () => <h1 className="Home">Select any menu item provided above</h1>
 
 export default Home;
