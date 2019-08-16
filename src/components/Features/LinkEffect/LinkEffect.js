b0f0b080cbe41caaa182654193532d6faeb3b68e
Konstantin <konstantin.v.modin@gmail.com>
2019-08-16 07:33:42 +0200

    updates

--- src/components/Features/LinkEffect/LinkEffect.js
+++ src/components/Features/LinkEffect/LinkEffect.js
@@ -2,9 +2,24 @@ import React from 'react';
 import './LinkEffect.css';
 
 const LinkEffect = () => {
+    const highlight = React.createRef();
+    
+    const highlightHandler = ({target}) => {
+        const linkCoords = target.getBoundingClientRect();
+        const {width, height, top, left } = linkCoords;
+        
+        highlight.current.style.top = offsetTop+"px";
+        highlight.current.style.left = offsetLeft+"px";
+        highlight.current.style.height = clientHeight+"px";
+        highlight.current.style.width = clientWidth+"px";
+    }
+
     return (
         <div className="LinkEffect Block2">
-            {Array.from(Array(12), (_,i) => <a href="/#" className="innerDiv">Link {++i}</a>)}            
+            {Array.from(Array(12), (_,i) => (
+                <a key={i} href="/#" className="innerDiv" onMouseEnter={highlightHandler}>Link {++i}</a>
+            ))}
+            <span className="highlight" ref={highlight}></span>            
         </div>
     )
 }
