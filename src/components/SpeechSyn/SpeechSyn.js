c603ddd236f451660b19879251cd1bef1260ff47
Konstantin <konstantin.v.modin@gmail.com>
2019-08-27 05:16:31 +0200

    updates

--- src/components/SpeechSyn/SpeechSyn.js
+++ src/components/SpeechSyn/SpeechSyn.js
@@ -1,21 +1,91 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './SpeechSyn.css';
 
-const SpeechSyn = () => {
+const SpeechSyn = () => {    
+    const [ start, setStart ] = useState(false);
+    const [ text, setText ] = useState("Hello! I love JavaScript!");
+    const [ voices, setVoices ] = useState([]);
+    const [ currentVoice, setCurrentVoice ] = useState(null);
+    const [ [rate, pitch], setOptions ] = useState([1, 1]);
+
+    const defineVoices = () => setVoices(speechSynthesis.getVoices());
+
+    useEffect(()=> {        
+        if (!currentVoice) setCurrentVoice(voices[0]);
+    // eslint-disable-next-line
+    }, [voices]);
+
+    useEffect(() => {        
+        speechSynthesis.addEventListener('voiceschanged', defineVoices);
+        return () => {
+            speechSynthesis.removeEventListener('voiceschanged', defineVoices);
+        }
+    }, []);
+    
+    useEffect(() => {
+        if (start) toggle();
+    // eslint-disable-next-line
+    }, [currentVoice, rate, pitch]);
+
+    const controlsHandler = ({ target: {name, value} }) => {
+        switch(name) {
+            case 'rate': setOptions([value, pitch]);
+            break;
+            case 'pitch': setOptions([rate, value]);
+            break;
+            case 'text': setText(value);
+            break;
+            default: return;
+        }
+    }    
+
+    const speakHandler = () => {
+        setStart(true);
+        const msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        msg.voice = currentVoice;
+        msg.pitch = pitch;
+        msg.rate = rate;        
+        speechSynthesis.speak(msg);
+    }   
+
+    const defineVoice = ({ target: {value}}) => {
+        setStart(true);
+        setCurrentVoice(voices.find(voice => voice.name === value));       
+    }
+    
+    const toggle = (play = true) => {
+        speechSynthesis.cancel();
+        if (play) speakHandler();
+    }
+
+    const enterHitedCheck = ( { key } ) => {        
+        if (key === 'Enter') {
+            setStart(true);
+            toggle();
+        }
+    }
+
     return (
         <div className="SpeechSyn">
             <h1>Speech Synthesis</h1>
-            <select className="Select">
-                <option>Select a Voice</option>
+            <select className="Select" onChange={defineVoice}>
+                {voices.map(({name, lang})=><option key={name+lang} value={name}>{name}(lang)</option>)}
             </select>
-            <label for="rate">Rate:</label>
-            <input className="Rate" name="rate" type="range" min={0} max={3} value={1} step={0.1}/>
-            <label for="pitch">Pitch:</label>
-            <input className="Pitch" name="pitch" type="range" min={0} max={2} step={0.1}/>
-            <textarea className="TextArea">Hello! I love JavaScript!</textarea>
+            <label htmlFor="rate">Rate: {rate}</label>
+            <input className="Rate" name="rate" type="range" onChange={controlsHandler} min={0} max={3} value={rate} step={0.1}/>
+            <label htmlFor="pitch">Pitch: {pitch}</label>
+            <input className="Pitch" name="pitch" type="range" onChange={controlsHandler} min={0} max={2} value={pitch} step={0.1}/>
+            <textarea
+                name="text" 
+                className="TextArea" 
+                onChange={controlsHandler} 
+                onKeyDown={(e) => enterHitedCheck(e.nativeEvent)} 
+                value={text}
+            />
             <div className="ButtonContainer">
-                <button className="StopButton btn">Stop</button>
-                <button className="SpeakButton btn">Speak</button>
+                <button className="StopButton btn" onClick={() => toggle(false)}>Stop</button>
+                <button className="SpeakButton btn" onClick={toggle}>Speak</button>
             </div>
         </div>
     )
