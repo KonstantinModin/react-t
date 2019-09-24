8a6e175f285cefc6657c2c6f57ac740b5db1c98f
Konstantin <konstantin.v.modin@gmail.com>
2019-09-24 15:05:36 +0200

    updates

---
+++ src/components/Features/Scroll/Scroll.js
@@ -0,0 +1,63 @@
+import React, { useState, createRef } from 'react';
+import './Scroll.css';
+
+const Scroll = () => {
+    const [ isDown, setIsDown ] = useState(false);
+    const [ startX, setStartX ] = useState(null);
+    const [ startY, setStartY ] = useState(null);
+    const [ scrollLeft, setScrollLeft ] = useState(null);
+    const [ persp, setPersp ] = useState(100);
+
+    const scrollDiv = createRef();
+
+    const handleMouseMove = (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        
+        //scroll left-right
+        const x = e.nativeEvent.pageX - scrollDiv.current.offsetLeft;
+        const walk = x - startX;
+        scrollDiv.current.scrollLeft = scrollLeft - walk*1;
+
+        //perspective
+        const notSoMuch = (a) => a < 40 ? 40 : a;
+        const y = e.nativeEvent.pageY - scrollDiv.current.offsetTop;
+        const walkY = y - startY;
+        scrollDiv.current.style.perspective = notSoMuch(persp - walkY*1)+'px'; 
+
+    }
+    const handleMouseDown = (e) => {
+        setIsDown(true);
+        //scroll left-right
+        setStartX(e.nativeEvent.pageX - scrollDiv.current.offsetLeft);
+        setScrollLeft(scrollDiv.current.scrollLeft);
+
+        //perspective
+        setStartY(e.nativeEvent.pageY - scrollDiv.current.offsetTop);
+        console.dir(scrollDiv.current.style.perspective);
+        setPersp(parseInt(scrollDiv.current.style.perspective));
+
+    }
+    const handleMouseUp = () => {
+        setIsDown(false);        
+    }
+
+    
+    return (
+        <div 
+            className={'Scroll' + (isDown? ' active' : '')}
+            onMouseDown={handleMouseDown}
+            onMouseLeave={() => setIsDown(false)}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+            ref={scrollDiv}
+            style={{perspective:'100px'}}
+        >
+            {[...Array(25)].map((e,i)=>{
+                return <div key={i} className={`item item${i+1}`}>{`${('0'+(i+1)).slice(-2)}`}</div>
+            })}     
+        </div>
+    )
+}
+
+export default Scroll;
