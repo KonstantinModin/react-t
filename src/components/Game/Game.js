f5bbeb6635478baa0e2a716832c51431e04cbf0d
Konstantin <konstantin.v.modin@gmail.com>
2019-09-27 06:36:03 +0200

    updates

---
+++ src/components/Game/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Game.css';
+
+const Game = () => {
+    return (
+        <div className="Game">
+            <h1>Game</h1>
+        </div>
+    )
+}
+
+export default Game;
