12a2814bd9af49f336a146a99f27247e9c483389
Konstantin <konstantin.v.modin@gmail.com>
2019-11-19 15:52:18 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <header className="App-header">
-            <div className="Logo"><NavLink to="/" ><h3>React App</h3></NavLink></div>
+            <div className="Logo"><Link to="/" ><h3>React App</h3></Link></div>
             <div className="Menu">
                 <NavLink exact activeClassName="selected" to="/" >Home</NavLink>       {/* TODO: Replace NavLinks with NavNavLinks  */}
                 <NavLink activeClassName="selected" to="/clock">Clock</NavLink>  {/*  add "Selected" class to menu Item */}
@@ -19,7 +19,9 @@ const Header = () => {
                 <NavLink activeClassName="selected" to="/videoplayer">Video Plr</NavLink>
                 <NavLink activeClassName="selected" to="/game">Game</NavLink>                
                 <NavLink activeClassName="selected" to="/misc">Misc</NavLink>
-                <NavLink activeClassName="selected" to="/hack/1">Hacker News</NavLink>
+                <NavLink exact={false} activeClassName="selected" to="/hack/1" isActive={(_, location)=>{                    
+                    return /^\/hack\/.+$/.test(location.pathname);
+                }}>Hacker News</NavLink>
                 <NavLink activeClassName="selected" to="/info">Info</NavLink>                
                 {/* <NavLink activeClassName="selected" to="/git">Git Issues</NavLink> */}
             </div>
