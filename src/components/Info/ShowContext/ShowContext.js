41756f599d9eb8838c7f6a1386673092302fd6b0
Konstantin <konstantin.v.modin@gmail.com>
2019-12-02 06:04:41 +0100

    updates

--- src/components/Info/ShowContext/ShowContext.js
+++ src/components/Info/ShowContext/ShowContext.js
@@ -7,11 +7,9 @@ import './ShowContext.css';
 const ShowContext = () => ( 
     <div className="ShowContext">
         <h3>ShowContext</h3>        
-        <header>You can update context in <Link to="/">Home Section</Link></header>
-        {/* <div className="table"> */}
-            <HooksComponent />
-            <ClassComponent />        
-        {/* </div>     */}
+        <header>You can update context in <Link to="/">Home Section</Link></header>        
+        <HooksComponent />
+        <ClassComponent />        
     </div>   
 )
 
