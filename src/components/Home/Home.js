86f8971544c51684129bde8e65d3fcec91aebdda
Konstantin <konstantin.v.modin@gmail.com>
2020-01-03 05:47:04 +0100

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = React.forwardRef((props, ref) => {    
     return (
         <div className="Home">
             <div><h1>Select any menu item provided above</h1></div>
-            <div>
-                <div className="description">Type here some text which will be forwarded to App.js with React.forawardRef 
-                and then will be provided as a Context to entire App.
-                </div>
+            <div className="description">
+                <div>
+                    Type here some text which will be forwarded 
+                    to App.js with React.forawardRef and then will be provided as a Context 
+                    to entire App. You will be able to see it <Link to="info">Test section </Link> 
+                    of this site.
+                </div>                
                 <input ref={ref} onChange={props.onInputClick} defaultValue={props.defaultValue}/>
             </div>
         </div>
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './ReduxFav/reducers/';
-import Menu from './Menu';
 import ShowContext from './ShowContext';
 import Hooks from './Hooks';
 import ReduxFav from './ReduxFav';
@@ -13,8 +12,7 @@ const store = createStore(reducer);
 const Info = () => (
     <div className="Info">
         <h1>Info</h1>
-        <div className="grid">
-            <Menu />
+        <div className="grid">            
             <ShowContext />
             <Hooks />
             <Provider store={store}><ReduxFav /></Provider>                
