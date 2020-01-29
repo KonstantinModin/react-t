7d7d9a31baae2bf2eef154dfcb355bd1835b0d39
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-29 07:16:17 +0100

    updates

--- src/components/Beer/redux/reducers/items.js
+++ src/components/Beer/redux/reducers/items.js
@@ -1,10 +1,14 @@
-import * as actionTypes from '../actionTypes.js';
+import { 
+    ADD_NEW_PAGE, 
+    FETCH_ITEM_REQUEST, 
+    FETCH_ITEM_SUCCESS,
+    FETCH_ITEM_FAILURE } from '../actionTypes.js';
 
 const INITIAL_STATE = [];
 
 const items = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case actionTypes.ADD_NEW_PAGE: {
+        case ADD_NEW_PAGE: {
             console.log('new page');
             const newPage = [...Array(25)].map(_=>({
                 shouldFetch: true,
@@ -13,9 +17,25 @@ const items = (state = INITIAL_STATE, action) => {
                 error: null
             }));            
             return [...state, ...newPage];
-        }
-        default: return state
-    }
+        };
+
+        case FETCH_ITEM_REQUEST: {
+            console.log('fetching...', action.id);            
+            return state.map((e,i)=>i!== action.id? e : {...e, loading:true, shouldFetch:false});
+        };
+
+        case FETCH_ITEM_SUCCESS: {
+            console.log('success', action.id);
+            return state;
+        };
+
+        case FETCH_ITEM_FAILURE: {
+            console.log('fail', action.id);
+            return state;
+        };
+
+        default: return state;
+    };
 };
 
 export default items;
