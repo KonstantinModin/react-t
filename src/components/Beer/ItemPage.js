37610288fb14f4dd340ab918311bc7adaf159f12
KonstantinModin <konstantin.v.modin@gmail.com>
2020-02-04 07:13:05 +0100

    updates

--- src/components/Beer/ItemPage.js
+++ src/components/Beer/ItemPage.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
 import { connect } from 'react-redux';
+import { Row, Col } from 'react-bootstrap';
 
 const ItemPage = ({ id, data }) => {   
-    console.log(data);
-    const { name, tagline, description, first_brewed, image_url } = data || {};    
+    const { name, tagline, description, first_brewed, image_url, abv, ibu, target_fg,
+        target_og, ebc, srm, ph, attenuation_level, food_pairing, brewers_tips,
+        contributed_by, ingredients:{ malt, hops, yeast } } = data || {};    
+    
+    console.log( malt, hops, yeast );
 
     return (
-        <div className="itemPage">            
-            <div className="title">
+        <Row className="itemPage">            
+            <Col md={2} className="img">
                 <img src={image_url} alt={tagline} />
+            </Col>
+            <Col md={10} className="content">
                 <h3>{name}</h3>
-            </div>
-            <h5>{tagline}</h5>
-            <p><span>First Brewed: </span>{first_brewed}</p>
-            <p>{description}</p>            
-            <button type="button" className="btn btn-outline-danger">Go back ...</button>
-        </div>
+                <h5>{tagline}</h5>
+                <p><span>First Brewed: </span>{first_brewed}</p>
+                <p>abv: {abv}, ibu: {ibu}, target_fg: {target_fg}, target_og: {target_og},
+                ebc: {ebc}, srm: {srm}, ph: {ph}, attenuation_level: {attenuation_level}</p>
+                <p>{description}</p>
+                <div>Food Pairing:
+                    <ul>
+                        {food_pairing.map(e=><li key={e}>{e}</li>)}
+                    </ul>
+                </div>
+                <p>Brewers tips: {brewers_tips}</p>
+                            
+                <button type="button" className="btn btn-outline-danger">Go back ...</button>
+            </Col>            
+        </Row>
     )
 }
 
