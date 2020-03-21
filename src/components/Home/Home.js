7aefad24d3276d68579bbd9f243dbc24ef6ba27b
KonstantinModin <konstantin.v.modin@gmail.com>
2020-03-21 20:44:28 +0100

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import portf from './portf.jpg';
-import './Home.css';
+import React from "react";
+import portf from "./portf.jpg";
+import "./Home.css";
 
-const Home = () => { 
+const Home = () => {
     return (
         <div className="Home">
             <div className="title">
-                <h1>Select any menu item provided above</h1>
-                <span className="finger" role="img" aria-label="finger up">☝</span>
+                <h2>Select any menu item provided above</h2>
+                <span className="finger" role="img" aria-label="finger up">
+                    ☝
+                </span>
             </div>
+            <h1>React Experiments</h1>
             <div className="portfolioLink">
-                <a className='link' href='https://cranky-hodgkin-28e9ed.netlify.com' target="_blank" rel="noopener noreferrer">
+                <a
+                    className="link"
+                    href="https://cranky-hodgkin-28e9ed.netlify.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img src={portf} alt="portf logo" />
-                    <h2>If you wanna visit my portfolio site please click here</h2>
+                    <h2>
+                        If you wanna visit my portfolio site please click here
+                    </h2>
                 </a>
             </div>
         </div>
-    )
+    );
 };
 
 export default Home;
